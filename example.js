d0fe13fb3ce94462e7614a30fe2cc10201d02360
148120Tran <nhattran1801@gmail.com>
2024-04-23 08:50:58 +0700

    feat:feature B

--- example.js
+++ example.js
@@ -2,6 +2,14 @@ const a = 1;
 const b = 2;
 const c = 3;
 
-function master () {
-    return a + b + c;
+function master() {
+  return a + b + c;
+}
+
+function branchCommon() {
+  if (c === 3) {
+    return c - b - a;
+  }
+
+  return 0;
 }
